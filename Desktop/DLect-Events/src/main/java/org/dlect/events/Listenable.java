b6686e9f5b695e080cc46d6875b49181cf50c424
Lee Symes <leesdolphin@users.sourceforge.net>
2014-02-07 22:57:39 +1000

    Added start of Listenable interface.

---
+++ Desktop/DLect-Events/src/main/java/org/dlect/events/Listenable.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.dlect.events;
+
+import javax.annotation.Nonnull;
+
+/**
+ *
+ * @author lee
+ * @param <T> The class that this class should use.
+ */
+public class Listenable<T extends Listenable<T>>{
+    
+    private EventAdapter e = EventAdapterBuilder.getNewAdapter();
+    
+    
+    public boolean addListener(@Nonnull EventListener l, Class<?>... listeningClasses) {
+        return e.addListener(l, listeningClasses);
+    }
+    
+    
+    public boolean removeListener(EventListener l) {
+        return e.removeListener(l);
+    }
+    
+    
+    protected void addChild(Listenable<?>... listenables) {
+        
+    }
+    
+}
