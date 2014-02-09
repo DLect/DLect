c4a711aa21107c93414acac9c9135470b54e47f1
Lee Symes <leesdolphin@users.sourceforge.net>
2014-02-09 20:28:37 +1000

    Added documentation to EventID. Added `isUniqueId` to determine if this Event corresponds to a change in the ID.

--- Desktop/DLect-Events/src/main/java/org/dlect/events/EventID.java
+++ Desktop/DLect-Events/src/main/java/org/dlect/events/EventID.java
@@ -6,12 +6,29 @@
 package org.dlect.events;
 
 /**
+ * This class represents a single type of event that can be fired. This class is expected to implement equality and as
+ * such it is recommended that implementations are enums, as that provides equality and name internally. If a class does
+ * not want to implement this using a enum then equality is defined on the combination {@link #getAppliedClass() } and
+ * {@link #getName() }. The {@link #isUniqueId() } is for applications to determine if they should update their internal
+ * IDs from this event.
  *
  * @author lee
  */
 public interface EventID {
 
+    /**
+     * The class that this EventID is associated with.
+     *
+     * @return The class that this event applies to.
+     */
     public Class<?> getAppliedClass();
 
-    public String getName();    
+    /**
+     * An easy to read name
+     *
+     * @return
+     */
+    public String getName();
+
+    public boolean isUniqueId();
 }
