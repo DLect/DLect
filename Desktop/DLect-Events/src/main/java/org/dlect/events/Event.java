f1d54a449bf1fa27551d957a527676e9230fe9a1
Lee Symes <leesdolphin@users.sourceforge.net>
2014-02-09 20:24:30 +1000

    Added `getSourceClass` method to standardise. Changed equals implementation. Added source type check.

--- Desktop/DLect-Events/src/main/java/org/dlect/events/Event.java
+++ Desktop/DLect-Events/src/main/java/org/dlect/events/Event.java
@@ -6,6 +6,7 @@
 package org.dlect.events;
 
 import java.util.Objects;
+import javax.annotation.Nonnull;
 
 /**
  *
@@ -18,13 +19,18 @@ public class Event {
     private final Object before;
     private final Object after;
 
-    public Event(Object source, EventID eventID, Object before, Object after) {
+    public Event(@Nonnull Object source, @Nonnull EventID eventID, Object before, Object after) {
         if (source == null) {
-            throw new IllegalArgumentException("Trying to fire an event with a null source");
+            throw new NullPointerException("Trying to fire an event with a null source");
         }
         if (eventID == null) {
-            throw new IllegalArgumentException("Trying to fire an event with a null eventID");
+            throw new NullPointerException("Trying to fire an event with a null eventID");
         }
+        if (!eventID.getAppliedClass().isAssignableFrom(source.getClass())) {
+            // If the event ID represents a supertype of or equal to source class; then good. Otherwise error.
+            throw new IllegalArgumentException("Source(" + source.getClass() + ") is not a sub-type of the class that the event ID(" + eventID.getAppliedClass() + ") applies to.");
+        }
+        // TODO consider adding check that getSource().getClass() == getEventID().getAppliedClass()
         this.source = source;
         this.eventID = eventID;
         this.before = before;
@@ -43,10 +49,7 @@ public class Event {
 
     @Override
     public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
+        if (!(obj instanceof Event)) {
             return false;
         }
         final Event other = (Event) obj;
@@ -60,6 +63,11 @@ public class Event {
             return false;
         }
         return Objects.equals(this.getAfter(), other.getAfter());
+    }
+
+    public Class<?> getSourceClass() {
+        // TODO decide between getSource().getClass() and getEventID().getAppliedClass()
+        return eventID.getAppliedClass();
     }
 
     public Object getSource() {
