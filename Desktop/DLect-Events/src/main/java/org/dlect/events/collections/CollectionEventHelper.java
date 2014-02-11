a533fdf90003db55b1398ece4937664fbf63f834
Lee Symes <leesdolphin@users.sourceforge.net>
2014-02-11 15:09:57 +1000

    Added documentation to CollectionEventHelper. Added null checks to constructor.

--- Desktop/DLect-Events/src/main/java/org/dlect/events/collections/CollectionEventHelper.java
+++ Desktop/DLect-Events/src/main/java/org/dlect/events/collections/CollectionEventHelper.java
@@ -8,14 +8,17 @@ package org.dlect.events.collections;
 import com.google.common.collect.Maps;
 import java.util.ConcurrentModificationException;
 import java.util.Map;
+import javax.annotation.Nonnull;
 import org.dlect.events.Event;
 import org.dlect.events.EventAdapter;
 import org.dlect.events.EventID;
 import org.dlect.events.ListEvent;
 
 /**
+ * A helper object that handles the actual event firing for collections. This object also prevents event handlers from
+ * modifying the same collection. Thus hopefully mitigating the risk of concurrent modification exceptions at a later
+ * time.
  *
- * @author lee
  * @param <T> The type of object that this class accepts.
  */
 public class CollectionEventHelper<T> {
@@ -26,8 +29,27 @@ public class CollectionEventHelper<T> {
 
     private final transient Map<Thread, Event> firingEvents = Maps.newHashMap();
 
-    public CollectionEventHelper(EventID event, Object source, EventAdapter adapter) {
-        this.eventID = event;
+    /**
+     * Creates a new helper using the given eventID, source and adapter. All events fired through this helper will be
+     * fired to the given event adapter and will be initialised with the given event ID and source.
+     *
+     * @param eventID The event ID of the events that this object will fire.
+     * @param source  The source of the events the this object will fire
+     * @param adapter The adapter to fire the events through.
+     *
+     * @throws IllegalArgumentException If any argument is null.
+     */
+    public CollectionEventHelper(@Nonnull EventID eventID, @Nonnull Object source, @Nonnull EventAdapter adapter) {
+        if (eventID == null) {
+            throw new IllegalArgumentException("EventID is null");
+        }
+        if (source == null) {
+            throw new IllegalArgumentException("Source is null");
+        }
+        if (adapter == null) {
+            throw new IllegalArgumentException("Event Adapter is null");
+        }
+        this.eventID = eventID;
         this.source = source;
         this.adapter = adapter;
     }
@@ -35,7 +57,7 @@ public class CollectionEventHelper<T> {
     protected void beginChange(Event toFire) {
         Thread t = Thread.currentThread();
         synchronized (firingEvents) {
-            if (firingEvents.containsKey(t)) {
+            if (isLocked(t)) {
                 throw new ConcurrentModificationException("Attempting to modify a collection from within an event handler is prohibited. Old Event: " + firingEvents.get(t) + "; New Event: " + toFire);
             } else {
                 firingEvents.put(t, toFire);
@@ -43,10 +65,14 @@ public class CollectionEventHelper<T> {
         }
     }
 
-    protected boolean isLocked() {
+    protected boolean isLocked(Thread t) {
         synchronized (firingEvents) {
-            return firingEvents.containsKey(Thread.currentThread());
+            return firingEvents.containsKey(t);
         }
+    }
+
+    protected boolean isLocked() {
+        return isLocked(Thread.currentThread());
     }
 
     protected void endChange() {
@@ -60,6 +86,16 @@ public class CollectionEventHelper<T> {
         }
     }
 
+    /**
+     * Fires an addition event to this object's adapter. This method must be called after the change has occurred in
+     * the list. This method enforces that not EventListener may modify the collection from inside the event handler
+     * handling an event from the associated collection.
+     *
+     * @param addedElement The object added to the associated collection
+     *
+     * @see ListEvent#getAddEvent(java.lang.Object, org.dlect.events.EventID, java.lang.Object)
+     * @see EventAdapter#fireEvent(org.dlect.events.Event)
+     */
     public void fireAdd(T addedElement) {
         final ListEvent event = ListEvent.getAddEvent(source, eventID, addedElement);
         beginChange(event);
@@ -70,6 +106,16 @@ public class CollectionEventHelper<T> {
         }
     }
 
+    /**
+     * Fires a replacement event to this object's adapter. This method must be called after the change has occurred in
+     * the list. This method enforces that not EventListener may modify the collection from inside the event handler
+     * handling an event from the associated collection.
+     *
+     * @param removedElement The object removed from the associated collection
+     *
+     * @see ListEvent#getRemoveEvent(java.lang.Object, org.dlect.events.EventID, java.lang.Object)
+     * @see EventAdapter#fireEvent(org.dlect.events.Event)
+     */
     public void fireRemove(T removedElement) {
         final ListEvent event = ListEvent.getRemoveEvent(source, eventID, removedElement);
         beginChange(event);
@@ -80,6 +126,17 @@ public class CollectionEventHelper<T> {
         }
     }
 
+    /**
+     * Fires a replacement event to this object's adapter. This method must be called after the change has occurred in
+     * the list. This method enforces that not EventListener may modify the collection from inside the event handler
+     * handling an event from the associated collection.
+     *
+     * @param original    The original object before it was replaced.
+     * @param replacement The object replacing the original object.
+     *
+     * @see ListEvent#getReplaceEvent(java.lang.Object, org.dlect.events.EventID, java.lang.Object, java.lang.Object)
+     * @see EventAdapter#fireEvent(org.dlect.events.Event)
+     */
     public void fireReplace(T original, T replacement) {
         final ListEvent event = ListEvent.getReplaceEvent(source, eventID, original, replacement);
         beginChange(event);
