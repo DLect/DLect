b10513024107d3e02c2b99160b653c3011b82c85
Lee Symes <leesdolphin@users.sourceforge.net>
2014-02-12 22:50:19 +1000

    Finished implementing EventFiringMapKeySetTest.

--- Desktop/DLect-Events/src/test/java/org/dlect/events/collections/EventFiringMapKeySetTest.java
+++ Desktop/DLect-Events/src/test/java/org/dlect/events/collections/EventFiringMapKeySetTest.java
@@ -5,14 +5,19 @@
  */
 package org.dlect.events.collections;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
@@ -161,57 +166,204 @@ public class EventFiringMapKeySetTest {
     /**
      * Test of iterator method, of class EventFiringMapKeySet.
      */
-    @Ignore
     @Test
     public void testIterator() {
+        Map<String, String> map = fillMap(1, 10);
+        Set<Entry<String, String>> toAdd = map.entrySet();
+
+        List<String> removed = Lists.newArrayList();
+
+        set.addAll(toAdd);
+
+        configureAnswer();
+
+        Iterator<String> it = testObject.iterator();
+        for (; it.hasNext();) {
+            String entry = it.next();
+
+            it.remove();
+            removed.add(entry);
+        }
+
+        InOrder inOrder = inOrder(helper);
+        for (String rem : removed) {
+            inOrder.verify(helper).fireRemove(Maps.immutableEntry(rem, map.get(rem)));
+        }
+
+        verifyNoMoreInteractions(helper);
     }
 
     /**
      * Test of remove method, of class EventFiringMapKeySet.
      */
-    @Ignore
     @Test
     public void testRemove() {
+        Map<String, String> map = fillMap(1, 10);
+        Set<Entry<String, String>> toAdd = map.entrySet();
+
+        List<String> removed = Lists.newArrayList();
+
+        set.addAll(toAdd);
+
+        configureAnswer();
+
+        for (Entry<String, String> entry : toAdd) {
+            String key = entry.getKey();
+
+            testObject.remove(key);
+
+            removed.add(key);
+        }
+
+        InOrder inOrder = inOrder(helper);
+        for (String rem : removed) {
+            inOrder.verify(helper).fireRemove(Maps.immutableEntry(rem, map.get(rem)));
+        }
+
+        verifyNoMoreInteractions(helper);
+    }
+
+    /**
+     * Test of remove method, of class EventFiringMapKeySet.
+     */
+    @Test
+    public void testRemove_No_Elements() {
+        configureAnswer();
+
+        testObject.remove("I don't exist");
+
+        verifyZeroInteractions(helper);
     }
 
     /**
      * Test of removeAll method, of class EventFiringMapKeySet.
      */
-    @Ignore
     @Test
     public void testRemoveAll() {
+        Map<String, String> map = fillMap(1, 10);
+        Set<Entry<String, String>> toAdd = map.entrySet();
+
+        Set<String> toRemove = fillKeySet(1, 5);
+
+        set.addAll(toAdd);
+
+        configureAnswer();
+
+        testObject.removeAll(toRemove);
+
+        for (String rem : toRemove) {
+            verify(helper).fireRemove(Maps.immutableEntry(rem, map.get(rem)));
+        }
+
+        verifyNoMoreInteractions(helper);
     }
 
     /**
      * Test of retainAll method, of class EventFiringMapKeySet.
      */
-    @Ignore
     @Test
     public void testRetainAll() {
+        Map<String, String> map = fillMap(1, 10);
+        Set<Entry<String, String>> toAdd = map.entrySet();
+
+        Set<String> toRetain = fillKeySet(1, 5);
+        Set<String> removed = Sets.newHashSet(map.keySet());
+        removed.removeAll(toRetain);
+
+        System.out.println("Removed List: " + removed);
+        
+        set.addAll(toAdd);
+
+        configureAnswer();
+
+        System.out.println("Set Before: " + set);
+
+        testObject.retainAll(toRetain);
+
+        System.out.println("Set After: " + set);
+        
+        for (String rem : removed) {
+            System.out.println("Removed: " + rem);
+            verify(helper).fireRemove(Maps.immutableEntry(rem, map.get(rem)));
+        }
+
+        verifyNoMoreInteractions(helper);
     }
 
     /**
      * Test of size method, of class EventFiringMapKeySet.
      */
-    @Ignore
     @Test
     public void testSize() {
+        configureAnswer();
+
+        assertEquals(0, testObject.size());
+
+        set.add(Maps.immutableEntry("Key", "Value"));
+
+        assertEquals(1, testObject.size());
+
+        verifyZeroInteractions(helper);
+    }
+
+    @Test
+    public void testSafeIterator() {
+        // This method is used internally as a cheeper iterator. It does not fire events.
+        Map<String, String> map = fillMap(1, 10);
+        Set<Entry<String, String>> toAdd = map.entrySet();
+
+        set.addAll(toAdd);
+
+        configureAnswer();
+
+        Iterator<String> it = testObject.safeIterator();
+        for (; it.hasNext();) {
+            it.next();
+            try {
+                it.remove();
+                fail("No Exception thrown");
+            } catch (UnsupportedOperationException e) {
+            }
+        }
+        verifyZeroInteractions(helper);
     }
 
     /**
      * Test of toArray method, of class EventFiringMapKeySet.
      */
-    @Ignore
     @Test
     public void testToArray_0args() {
+        Map<String, String> map = fillMap(1, 10);
+        Set<Entry<String, String>> toAdd = map.entrySet();
+
+        set.addAll(toAdd);
+
+        configureAnswer();
+
+        Object[] it = testObject.toArray();
+
+        assertEquals(map.keySet(), ImmutableSet.copyOf(it));
+
+        verifyZeroInteractions(helper);
     }
 
     /**
      * Test of toArray method, of class EventFiringMapKeySet.
      */
-    @Ignore
     @Test
     public void testToArray_GenericType() {
+        Map<String, String> map = fillMap(1, 10);
+        Set<Entry<String, String>> toAdd = map.entrySet();
+
+        set.addAll(toAdd);
+
+        configureAnswer();
+
+        String[] it = testObject.toArray(new String[1]);
+
+        assertEquals(map.keySet(), ImmutableSet.copyOf(it));
+
+        verifyZeroInteractions(helper);
     }
 
     private class AnswerImpl implements Answer<Void> {
