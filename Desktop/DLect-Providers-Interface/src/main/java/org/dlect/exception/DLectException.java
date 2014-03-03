326e6da1eb1f7434b58a2b010edac5b40e315210
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-03 22:34:30 +1000

    Custom DLectException with a custom cause Enum. Equivalent to the ActionResult

---
+++ Desktop/DLect-Providers-Interface/src/main/java/org/dlect/exception/DLectException.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dlect.exception;
+
+/**
+ *
+ * @author lee
+ */
+public class DLectException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    private final DLectExceptionCause causeCode;
+
+    public DLectException(DLectExceptionCause causeCode) {
+        this.causeCode = causeCode;
+    }
+
+    public DLectException(DLectExceptionCause causeCode, String message) {
+        super(message);
+        this.causeCode = causeCode;
+    }
+
+    public DLectException(DLectExceptionCause causeCode, Throwable cause) {
+        super(cause);
+        this.causeCode = causeCode;
+    }
+
+    public DLectException(DLectExceptionCause causeCode, String message, Throwable cause) {
+        super(message, cause);
+        this.causeCode = causeCode;
+    }
+
+    public DLectExceptionCause getCauseCode() {
+        return causeCode;
+    }
+
+    @Override
+    public String toString() {
+        String s = getClass().getName();
+        String message = getLocalizedMessage();
+        return (message != null) ? (s + " (" + causeCode + "): " + message) : s + " (" + causeCode + ")";
+    }
+    
+    
+
+}
