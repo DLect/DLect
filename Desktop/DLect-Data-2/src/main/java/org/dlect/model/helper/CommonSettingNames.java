ea984ba27e36aa3414c3b227aa50eee604c9c112
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-03 21:33:43 +1000

    Added a class containing common setting names.

---
+++ Desktop/DLect-Data-2/src/main/java/org/dlect/model/helper/CommonSettingNames.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dlect.model.helper;
+
+/**
+ *
+ * @author lee
+ */
+public class CommonSettingNames {
+
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String PROVIDER_CODE = "provider-code";
+
+}
