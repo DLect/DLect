65091d2b3b404ee760cf74d46ea746cc571354c6
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-03 23:35:33 +1000

    Added controller event and associated objects & helpers. Added enums for controller type and state

---
+++ Desktop/DLect-UI-Base/src/main/java/org/dlect/controller/event/ControllerType.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dlect.controller.event;
+
+import org.dlect.controller.helper.Controller;
+import org.dlect.events.EventID;
+
+/**
+ *
+ * @author lee
+ */
+public enum ControllerType implements EventID {
+
+    LOGIN, SUBJECT, LECTURE;
+
+    @Override
+    public Class<?> getAppliedClass() {
+        return Controller.class; // TODO fix this.
+    }
+
+    @Override
+    public String getName() {
+        return name();
+    }
+
+    @Override
+    public boolean isUniqueId() {
+        return false;
+    }
+
+}
