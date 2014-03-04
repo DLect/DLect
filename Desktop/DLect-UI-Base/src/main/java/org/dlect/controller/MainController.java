f03cbc67c1c59dc0c253aff2057364dede828eb9
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-04 20:40:23 +1000

    Added state helper to the children of the main controller so that they can recieve the events.

--- Desktop/DLect-UI-Base/src/main/java/org/dlect/controller/MainController.java
+++ Desktop/DLect-UI-Base/src/main/java/org/dlect/controller/MainController.java
@@ -42,7 +42,7 @@ public class MainController extends Listenable<MainController> implements Initil
         this.lectureController = new LectureController(this);
         this.controllerStateHelper = new ControllerStateHelper(this);
 
-        this.addChild(databaseHandler, loginController, subjectController, lectureController);
+        this.addChild(databaseHandler, loginController, subjectController, lectureController, controllerStateHelper);
         Initilisables.doInit(databaseHandler, providerHelper, loginController, subjectController, lectureController, controllerStateHelper);
     }
 
