2241c1862555a0df85e852efe53a407467b308f6
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-06 23:04:03 +1000

    Moved utility code for Provider subjects into a seperate helper class.

--- Desktop/DLect-Providers-Base/src/main/java/org/dlect/provider/WrappedProvider.java
+++ Desktop/DLect-Providers-Base/src/main/java/org/dlect/provider/WrappedProvider.java
@@ -7,29 +7,22 @@ package org.dlect.provider;
 
 import com.google.common.base.Objects;
 import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
-import com.google.common.collect.Sets;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 import org.dlect.encryption.DatabaseDecryptionHandler;
 import org.dlect.exception.DLectException;
 import org.dlect.exception.DLectExceptionCause;
-import org.dlect.immutable.model.ImmutableLecture;
 import org.dlect.immutable.model.ImmutableSemester;
-import org.dlect.immutable.model.ImmutableStream;
 import org.dlect.immutable.model.ImmutableSubject;
 import org.dlect.model.Database;
 import org.dlect.model.Semester;
 import org.dlect.model.Subject;
 import org.dlect.provider.helper.WrappedProviderLectureHelper;
+import org.dlect.provider.helper.WrappedProviderSubjectHelper;
+import org.dlect.provider.objects.ImmutableSubjectData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.dlect.model.helper.CommonSettingNames.*;
-import static org.dlect.provider.helper.WrappedProviderLectureHelper.creatingMapping;
 
 /**
  *
@@ -38,8 +31,7 @@ import static org.dlect.provider.helper.WrappedProviderLectureHelper.creatingMap
 public class WrappedProvider {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(WrappedProvider.class);
-    private static final Multimap<ImmutableLecture, ImmutableStream> EMPTY_LECTURE_STREAM_MAP = ImmutableMultimap.of();
-
+    
     private final Database d;
     private final DatabaseDecryptionHandler deh;
     private final Provider p;
@@ -99,59 +91,8 @@ public class WrappedProvider {
     public void getSubjects() throws DLectException {
         Multimap<ImmutableSemester, ImmutableSubject> subjects = p.getSubjectProvider().getSubjects();
 
-        insertSemestersIntoDatabase(subjects);
+        WrappedProviderSubjectHelper.insertSemestersIntoDatabase(d, subjects);
     }
 
-    private void insertSemestersIntoDatabase(Multimap<ImmutableSemester, ImmutableSubject> subjects) {
-        Map<Semester, Semester> semMap = creatingMapping(d.getSemesters());
-        
-        
-        Set<Semester> semesters = Sets.newHashSet();
-
-        for (Entry<ImmutableSemester, Collection<ImmutableSubject>> entry : subjects.asMap().entrySet()) {
-            ImmutableSemester sem = entry.getKey();
-            Collection<ImmutableSubject> collection = entry.getValue();
-
-            Semester s = sem.copyToNew();
-
-            Semester existing = semMap.get(s);
-            if (existing == null) {
-                existing = s;
-            } else {
-                sem.copyTo(existing);
-            }
-
-            semesters.add(existing);
-
-            insertSubjectsIntoSemester(existing, collection);
-        }
-        d.setSemesters(semesters);
-    }
-
-    private void insertSubjectsIntoSemester(Semester sem, Collection<ImmutableSubject> imSub) {
-        Map<Subject, Subject> subMap = creatingMapping(sem.getSubject());
-
-        Set<Subject> subjects = Sets.newHashSet();
-        for (ImmutableSubject is : imSub) {
-            Subject s = is.copyToNew();
-
-            Subject existing = subMap.get(s);
-            if (existing == null) {
-                existing = s;
-            } else {
-                is.copyTo(existing);
-            }
-
-            subjects.add(existing);
-
-            if (!is.getLectures().isEmpty() || !is.getStreams().isEmpty()) {
-                WrappedProviderLectureHelper.mergeSubjectData(existing,
-                                                              new ImmutableSubjectData(EMPTY_LECTURE_STREAM_MAP,
-                                                                                       is.getLectures(),
-                                                                                       is.getStreams()));
-            }
-        }
-        sem.setSubject(subjects);
-    }
 
 }
