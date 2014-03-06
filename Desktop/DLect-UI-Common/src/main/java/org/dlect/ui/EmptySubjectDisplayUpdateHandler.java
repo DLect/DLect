dfbee8ebf96b95d0f63ccacd85a25c66ce56d743
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-06 23:16:20 +1000

    Fixed potential bug with Update handler trying to get the last element out of an empty list.

--- Desktop/DLect-UI-Common/src/main/java/org/dlect/ui/EmptySubjectDisplayUpdateHandler.java
+++ Desktop/DLect-UI-Common/src/main/java/org/dlect/ui/EmptySubjectDisplayUpdateHandler.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.dlect.ui;
 
 import org.dlect.model.Database;
@@ -22,9 +21,13 @@ public class EmptySubjectDisplayUpdateHandler extends SubjectDisplayUpdateHandle
     @Override
     public void updateSubjects() {
         // Last semeseter is the most recent.
+        if (getDatabase().getSemesters().isEmpty()) {
+            // Nothing to update so don't try.
+            return;
+        }
         Semester newest = getDatabase().getSemesters().last();
         System.out.println("Updating: " + newest);
-        setSemesterEnabled(newest, true);    
+        setSemesterEnabled(newest, true);
     }
-    
+
 }
