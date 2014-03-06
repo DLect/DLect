dcf654af294b0e75182ecdb0d0d7666bc787c091
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-06 22:53:08 +1000

    Moved Provider helper objects into seperate package.

--- Desktop/DLect-Providers-Interface/src/main/java/org/dlect/provider/objects/ImmutableSubjectData.java
+++ Desktop/DLect-Providers-Interface/src/main/java/org/dlect/provider/objects/ImmutableSubjectData.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.dlect.provider;
+package org.dlect.provider.objects;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
