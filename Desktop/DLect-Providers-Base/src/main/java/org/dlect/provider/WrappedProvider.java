1a48ec316e01ef22567da682ad819d6eecd5de86
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-07 16:53:44 +1000

    Implemented Download in `WrappedProvider`.

--- Desktop/DLect-Providers-Base/src/main/java/org/dlect/provider/WrappedProvider.java
+++ Desktop/DLect-Providers-Base/src/main/java/org/dlect/provider/WrappedProvider.java
@@ -8,12 +8,24 @@ package org.dlect.provider;
 import com.google.common.base.Objects;
 import com.google.common.base.Optional;
 import com.google.common.collect.Multimap;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import org.dlect.encryption.DatabaseDecryptionHandler;
 import org.dlect.exception.DLectException;
 import org.dlect.exception.DLectExceptionCause;
+import org.dlect.file.FileController;
+import org.dlect.immutable.model.ImmutableLectureDownload;
 import org.dlect.immutable.model.ImmutableSemester;
 import org.dlect.immutable.model.ImmutableSubject;
 import org.dlect.model.Database;
+import org.dlect.model.Lecture;
+import org.dlect.model.LectureDownload;
 import org.dlect.model.Semester;
 import org.dlect.model.Subject;
 import org.dlect.provider.helper.WrappedProviderLectureHelper;
@@ -22,6 +34,7 @@ import org.dlect.provider.objects.ImmutableSubjectData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.dlect.helper.Conditions.checkNonNull;
 import static org.dlect.model.helper.CommonSettingNames.*;
 
 /**
@@ -31,21 +44,20 @@ import static org.dlect.model.helper.CommonSettingNames.*;
 public class WrappedProvider {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(WrappedProvider.class);
-    
+
     private final Database d;
     private final DatabaseDecryptionHandler deh;
+    private final FileController fc;
     private final Provider p;
 
-    public WrappedProvider(Provider p, Database d) {
-        if (p == null) {
-            throw new IllegalArgumentException("Provider is null");
-        }
-        if (d == null) {
-            throw new IllegalArgumentException("Database is null");
-        }
+    public WrappedProvider(Provider p, Database d, FileController fc) {
+        checkNonNull(p, "Provider");
+        checkNonNull(d, "Database");
+        checkNonNull(fc, "File Controller");
         this.p = p;
         this.d = d;
         this.deh = new DatabaseDecryptionHandler(d);
+        this.fc = fc;
     }
 
     public void doLogin() throws DLectException {
@@ -59,7 +71,6 @@ public class WrappedProvider {
         LoginProvider lp = p.getLoginProvider();
 
         lp.doLogin(usr.get(), pwd.get());
-
     }
 
     protected boolean findSubject(Subject s) {
@@ -94,5 +105,39 @@ public class WrappedProvider {
         WrappedProviderSubjectHelper.insertSemestersIntoDatabase(d, subjects);
     }
 
+    public void doDownload(Subject s, Lecture l, LectureDownload ld) throws DLectException {
+        // TODO ensure that subject, lecture and lecture download exist in the database.
+
+        File outputTo = fc.getFileForDownload(s, l, ld);
+        InputStream is = new BufferedInputStream(p.getDownloadProvider().getDownloadStreamFor(ImmutableLectureDownload.from(ld)));
+        OutputStream os;
+        try {
+            os = new BufferedOutputStream(new FileOutputStream(outputTo));
+        } catch (FileNotFoundException ex) {
+            throw new DLectException(DLectExceptionCause.INVALID_DATA_FORMAT, "The file was not found. "
+                                                                              + "This is a problem with "
+                                                                              + "getFileForDownload(" + s
+                                                                              + ", " + l + ", " + ld + ")", ex);
+        }
+        byte[] load = new byte[8192];
+        int lastRead;
+        int totalRead = 0;
+        do {
+            try {
+                lastRead = is.read(load);
+            } catch (IOException e) {
+                throw new DLectException(DLectExceptionCause.NO_CONNECTION, "Failed to read from download stream.", e);
+            }
+            if (lastRead < 0) {
+                break;
+            }
+            totalRead += lastRead;
+            try {
+                os.write(load, 0, lastRead);
+            } catch (IOException ex) {
+                throw new DLectException(DLectExceptionCause.INVALID_DATA_FORMAT);
+            }
+        } while (lastRead >= 0);
+    }
 
 }
