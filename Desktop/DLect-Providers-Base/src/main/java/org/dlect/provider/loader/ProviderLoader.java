6bd782c63e5a32de0f01a32fdd90a582296c33a9
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-07 19:36:03 +1000

    Added FileController as a parameter of loadProvider. Cleaned null checks.

--- Desktop/DLect-Providers-Base/src/main/java/org/dlect/provider/loader/ProviderLoader.java
+++ Desktop/DLect-Providers-Base/src/main/java/org/dlect/provider/loader/ProviderLoader.java
@@ -9,6 +9,8 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import org.dlect.file.FileController;
+import org.dlect.helper.Conditions;
 import org.dlect.model.Database;
 import org.dlect.provider.Provider;
 import org.dlect.provider.WrappedProvider;
@@ -49,17 +51,14 @@ public class ProviderLoader {
         return details;
     }
 
-    public WrappedProvider loadProvider(ProviderDetail pd, Database d) throws ExecutionException {
-        if (pd == null) {
-            throw new IllegalArgumentException("Null provider detail given");
-        }
-        if (d == null) {
-            throw new IllegalArgumentException("Null Database given");
-        }
+    public WrappedProvider loadProvider(ProviderDetail pd, Database d, FileController fc) throws ExecutionException {
+        Conditions.checkNonNull(pd, "Provider Detail");
+        Conditions.checkNonNull(d, "Database");
+        Conditions.checkNonNull(fc, "File Controller");
         try {
             Provider p = pd.getProviderClass().newInstance();
 
-            WrappedProvider wp = new WrappedProvider(p, d);
+            WrappedProvider wp = new WrappedProvider(p, d, fc);
 
             return wp;
         } catch (InstantiationException | IllegalAccessException ex) {
