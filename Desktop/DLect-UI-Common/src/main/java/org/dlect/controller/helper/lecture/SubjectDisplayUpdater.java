8e29fb16b61ca8c5dbe29337cd2b43aaad894a93
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-08 01:13:43 +1000

    Copies Subject display updater code to provide the basis for the lecture implementation.

---
+++ Desktop/DLect-UI-Common/src/main/java/org/dlect/controller/helper/lecture/SubjectDisplayUpdater.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dlect.controller.helper.lecture;
+
+import org.dlect.controller.MainController;
+import org.dlect.controller.event.ControllerEvent;
+import org.dlect.controller.event.ControllerState;
+import org.dlect.controller.event.ControllerType;
+import org.dlect.controller.helper.Controller;
+import org.dlect.events.Event;
+import org.dlect.events.EventListener;
+import org.dlect.model.Database;
+
+/**
+ *
+ * @author lee
+ */
+public class SubjectDisplayUpdater implements EventListener {
+
+    private final Database d;
+    private SubjectDisplayUpdateHandler sduh = null;
+
+    protected SubjectDisplayUpdater(Database d) {
+        // TODO implement this for LectureWorker.
+        this.d = d;
+    }
+
+    @Override
+    public void processEvent(Event e) {
+        if (e instanceof ControllerEvent) {
+            ControllerEvent ce = (ControllerEvent) e;
+            if (ce.getEventID() == ControllerType.SUBJECT) {
+                if (ce.getAfter() == ControllerState.STARTED) {
+                    if (d.getSemesters().isEmpty()) {
+                        sduh = new EmptySubjectDisplayUpdateHandler(d);
+                    } else {
+                        sduh = new UpdatingSubjectDisplayUpdateHandler(d);
+                    }
+                } else {
+                    if (sduh != null) {
+                        sduh.updateSubjects();
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * All instances of this class are equal. This returns true iff the object is the same type as this class.
+     *
+     * @param o {@inheritDoc }
+     *
+     * @return {@inheritDoc }
+     */
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof SubjectDisplayUpdater;
+    }
+
+    /**
+     * All instances of this class are equal, so all hash codes are the same.
+     *
+     * @return A pre-defined hash code.
+     */
+    @Override
+    public int hashCode() {
+        return 1029456435;
+    }
+
+    public static void registerOn(MainController mc) {
+        SubjectDisplayUpdater sdu = new SubjectDisplayUpdater(mc.getDatabaseHandler().getDatabase());
+        mc.addListener(sdu, Controller.class);
+    }
+
+}
---
+++ Desktop/DLect-UI-Common/src/main/java/org/dlect/controller/helper/lecture/UpdatingSubjectDisplayUpdateHandler.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dlect.controller.helper.lecture;
+
+import com.google.common.collect.ImmutableSortedSet.Builder;
+import com.google.common.collect.ImmutableSortedSet;
+import java.util.SortedSet;
+import org.dlect.model.Database;
+import org.dlect.model.Semester;
+
+/**
+ *
+ * @author lee
+ */
+public class UpdatingSubjectDisplayUpdateHandler extends SubjectDisplayUpdateHandler {
+
+    private final SortedSet<Semester> enabledSemesters;
+    private final int mostRecentSemesterCode;
+
+    public UpdatingSubjectDisplayUpdateHandler(Database d) {
+        super(d);
+        Builder<Semester> builder = ImmutableSortedSet.naturalOrder();
+        int sem = Integer.MIN_VALUE;
+
+        for (Semester semester : d.getSemesters()) {
+            if (semester.getNum() > sem) {
+                sem = semester.getNum();
+            }
+            if (isSemesterEnabled(semester)) {
+                builder.add(semester);
+            }
+        }
+        this.enabledSemesters = builder.build();
+        this.mostRecentSemesterCode = sem;
+    }
+
+    @Override
+    public void updateSubjects() {
+        for (Semester s : getDatabase().getSemesters()) {
+
+        }
+
+    }
+
+}
