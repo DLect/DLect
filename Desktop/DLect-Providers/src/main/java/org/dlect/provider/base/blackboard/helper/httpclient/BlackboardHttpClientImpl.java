2bd07f662a8470be68fdc42e0c76dbff5f48419b
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-11 00:33:43 +1000

    Moved HttpClient stuff into seperate package.

--- Desktop/DLect-Providers/src/main/java/org/dlect/provider/base/blackboard/helper/httpclient/BlackboardHttpClientImpl.java
+++ Desktop/DLect-Providers/src/main/java/org/dlect/provider/base/blackboard/helper/httpclient/BlackboardHttpClientImpl.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.dlect.provider.base.blackboard.helper;
+package org.dlect.provider.base.blackboard.helper.httpclient;
 
 import com.google.common.base.Charsets;
 import com.google.common.collect.Lists;
