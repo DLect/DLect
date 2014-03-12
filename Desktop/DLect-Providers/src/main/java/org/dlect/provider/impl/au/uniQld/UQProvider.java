f10a9e9a7b2b03d238b43ebdc58b7dbf4b548895
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-13 09:18:37 +1000

    Added default stream as UQ Rota is not implemented yet

--- Desktop/DLect-Providers/src/main/java/org/dlect/provider/impl/au/uniQld/UQProvider.java
+++ Desktop/DLect-Providers/src/main/java/org/dlect/provider/impl/au/uniQld/UQProvider.java
@@ -5,11 +5,14 @@
  */
 package org.dlect.provider.impl.au.uniQld;
 
+import com.google.common.collect.ImmutableSet;
 import java.util.Date;
 import java.util.Set;
 import org.dlect.immutable.model.ImmutableSemester;
 import org.dlect.immutable.model.ImmutableStream;
+import org.dlect.immutable.model.ImmutableSubject;
 import org.dlect.provider.base.blackboard.BlackboardBaseProvider;
+import org.dlect.provider.base.blackboard.helper.provider.BlackboardProviderInformation;
 import org.dlect.provider.impl.au.uniQld.rota.UQRotaHelper;
 
 /**
@@ -18,28 +21,36 @@ import org.dlect.provider.impl.au.uniQld.rota.UQRotaHelper;
  */
 public class UQProvider extends BlackboardBaseProvider {
 
+    private static final BlackboardProviderInformation UQ_PROVIDER_INFORMATION
+                                                       = new BlackboardProviderInformation(921,
+                                                                                           "https://learn.uq.edu.au/webapps/Bb-mobile-BBLEARN/",
+                                                                                           false,
+                                                                                           true);
+
     public UQProvider() {
         this(new UQRotaHelper() {
 
+            private final ImmutableStream STREAM = new ImmutableStream("Unknown");
+
             @Override
             public ImmutableSemester getSemester(int semCode) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                return new ImmutableSemester(semCode, "Sem Code: " + semCode, "" + semCode, ImmutableSet.<ImmutableSubject>of());
             }
 
             @Override
             public Set<ImmutableStream> getStreamsFor(String subjectCode, int semCode) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                return ImmutableSet.of(STREAM);
             }
 
             @Override
             public Set<ImmutableStream> getStreamsFor(String subjectCode, int semCode, Date lectureTime, String lectureRoom) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                return ImmutableSet.of(STREAM);
             }
         });
     }
 
     public UQProvider(UQRotaHelper helper) {
-        super(921, new UQSubjectCustomiser(helper), new UQLectureCustomiser(helper));
+        super(UQ_PROVIDER_INFORMATION, new UQSubjectCustomiser(helper), new UQLectureCustomiser(helper));
     }
 
 }
