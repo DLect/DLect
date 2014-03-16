469cd715dd7820b37c1f90997e9bbbd36b05dd16
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-16 14:37:02 +1000

    Moved SubjectDataHelper into helper.subject package to make it clearer.

--- Desktop/DLect-UI-Base/src/main/java/org/dlect/controller/helper/subject/SubjectDataHelper.java
+++ Desktop/DLect-UI-Base/src/main/java/org/dlect/controller/helper/subject/SubjectDataHelper.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.dlect.controller.helper;
+package org.dlect.controller.helper.subject;
 
 import com.google.common.base.Objects;
 import com.google.common.cache.CacheBuilder;
