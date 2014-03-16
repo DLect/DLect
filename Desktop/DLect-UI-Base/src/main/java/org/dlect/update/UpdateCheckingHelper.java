ae8500854900a837d1f64875896741cbb9ec12ef
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-16 18:11:35 +1000

    Implemented update checking helper to perform the update check and to launch the updater if required.

--- Desktop/DLect-UI-Base/src/main/java/org/dlect/update/UpdateCheckingHelper.java
+++ Desktop/DLect-UI-Base/src/main/java/org/dlect/update/UpdateCheckingHelper.java
@@ -6,6 +6,9 @@
 package org.dlect.update;
 
 import javax.annotation.Nonnull;
+import org.dlect.controller.MainController;
+import org.dlect.model.Database;
+import org.dlect.model.helper.CommonSettingNames;
 
 /**
  *
@@ -13,10 +16,32 @@ import javax.annotation.Nonnull;
  */
 public class UpdateCheckingHelper {
 
+    private final MainController mc;
+
+    private final OnlineUpdateChecker updateChecker;
+
+    private final UpdateExecutor updateExecutor;
+
+    public UpdateCheckingHelper(MainController mc) {
+        this(mc, new OnlineUpdateCheckerImpl(), new UpdateExecutorImpl());
+    }
+
+    public UpdateCheckingHelper(MainController mc, OnlineUpdateChecker updateChecker, UpdateExecutor updateExecutor) {
+        this.updateChecker = updateChecker;
+        this.mc = mc;
+        this.updateExecutor = updateExecutor;
+    }
+
     public void doUpdate(@Nonnull UpdateStyle us) throws UpdateException {
-        
-        
-        
+        System.out.println("Do Update: " + us);
+        final Database db = mc.getDatabaseHandler().getDatabase();
+        boolean update = updateChecker.isUpdateAvaliable(db.getSetting(CommonSettingNames.PROVIDER_CODE),
+                                                         db.getSetting(CommonSettingNames.UUID),
+                                                         db.getSetting(CommonSettingNames.BBID));
+
+        if (update) {
+            updateExecutor.executeUpdate(us);
+        }
     }
 
 }
