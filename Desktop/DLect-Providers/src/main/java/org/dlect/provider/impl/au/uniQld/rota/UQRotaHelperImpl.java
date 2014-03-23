7f4f6cf74a84f39d57bce6cb675f4eebbef88263
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-23 17:18:14 +1000

    Added additional constructors that correctly initilise the semester parser.

--- Desktop/DLect-Providers/src/main/java/org/dlect/provider/impl/au/uniQld/rota/UQRotaHelperImpl.java
+++ Desktop/DLect-Providers/src/main/java/org/dlect/provider/impl/au/uniQld/rota/UQRotaHelperImpl.java
@@ -12,16 +12,26 @@ import org.dlect.exception.DLectException;
 import org.dlect.immutable.model.ImmutableSemester;
 import org.dlect.immutable.model.ImmutableStream;
 import org.dlect.logging.ProviderLogger;
+import org.dlect.provider.base.blackboard.helper.httpclient.BlackboardHttpClient;
+import org.dlect.provider.base.blackboard.helper.httpclient.BlackboardHttpClientImpl;
 
 public class UQRotaHelperImpl implements UQRotaHelper {
-
+    
     private static final ImmutableStream STREAM = new ImmutableStream("Unknown");
     private final UQRotaSemesterParser semesterParser;
-
+    
     public UQRotaHelperImpl() {
-        semesterParser = null; // TODO
+        this(new BlackboardHttpClientImpl());
     }
-
+    
+    public UQRotaHelperImpl(BlackboardHttpClient client) {
+        this(new UQRotaSemesterParser(client));
+    }
+    
+    public UQRotaHelperImpl(UQRotaSemesterParser semesterParser) {
+        this.semesterParser = semesterParser;
+    }
+    
     @Override
     public ImmutableSemester getSemester(int semCode) {
         try {
@@ -32,15 +42,15 @@ public class UQRotaHelperImpl implements UQRotaHelper {
             return new ImmutableSemester(semCode, "Sem Code: " + semCode, Integer.toString(semCode));
         }
     }
-
+    
     @Override
     public Set<ImmutableStream> getStreamsFor(String subjectCode, int semCode) {
         return ImmutableSet.of(STREAM);
     }
-
+    
     @Override
     public Set<ImmutableStream> getStreamsFor(String subjectCode, int semCode, Date lectureTime, String lectureRoom) {
         return ImmutableSet.of(STREAM);
     }
-
+    
 }
