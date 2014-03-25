f810d12ab00ef34d0c38a6a223e3531330d7ab5f
Lee Symes <leesdolphin@users.sourceforge.net>
2014-03-25 11:02:36 +1000

    Updated Offering parser to return a value.

--- Desktop/DLect-Providers/src/main/java/org/dlect/provider/impl/au/uniQld/rota/UQRotaOfferingParser.java
+++ Desktop/DLect-Providers/src/main/java/org/dlect/provider/impl/au/uniQld/rota/UQRotaOfferingParser.java
@@ -5,17 +5,14 @@
  */
 package org.dlect.provider.impl.au.uniQld.rota;
 
-import com.google.common.base.Charsets;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
-import java.util.List;
+import java.util.Set;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import org.dlect.exception.DLectException;
 import org.dlect.exception.DLectExceptionCause;
-import org.dlect.helpers.FileDebuggingHelper;
 import org.dlect.logging.ProviderLogger;
 import org.dlect.provider.base.blackboard.helper.httpclient.BlackboardHttpClient;
 import org.dlect.provider.base.blackboard.helper.httpclient.BlackboardHttpClientImpl;
@@ -39,19 +36,16 @@ public class UQRotaOfferingParser {
         this.client = blackboardHttpClient;
     }
 
-    public List<UQRotaStream> getStreamsFor(int offeringID) throws DLectException {
+    public Set<UQRotaStream> getStreamsFor(int offeringID) throws DLectException {
         URI u = URI.create("http://rota.eait.uq.edu.au/offering/" + offeringID + ".xml");
-        
+
         UQRotaOfferingParseHandler handler = new UQRotaOfferingParseHandler();
         try {
             SAXParser saxParser = PARSER_FACTORY.newSAXParser();
 
-            FileDebuggingHelper.debugReaderToLogger(new InputStreamReader(client.doGet(u), Charsets.UTF_8), "O" + offeringID, ProviderLogger.LOGGER);
-            
             saxParser.parse(client.doGet(u), handler);
 
-            handler.printStuffs();
-            // return handler.getSemester();
+            return handler.getStreams();
         } catch (ParserConfigurationException ex) {
             ProviderLogger.LOGGER.error("Failed to configure parser whilst attempting to read " + u, ex);
             throw new DLectException(DLectExceptionCause.ILLEGAL_PROVIDER_STATE, "Failed to configure parser.", ex);
@@ -59,7 +53,6 @@ public class UQRotaOfferingParser {
             ProviderLogger.LOGGER.error("Failed to load xml from " + u, ex);
             throw new DLectException(DLectExceptionCause.ILLEGAL_SERVICE_RESPONCE, "Failed to load xml from UQ Rota url: " + u, ex);
         }
-        return null;
     }
 
 }
