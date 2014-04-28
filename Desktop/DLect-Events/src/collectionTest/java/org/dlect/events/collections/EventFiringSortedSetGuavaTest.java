9dbaa223c171771de6452bc9dad3776ac93af56f
Lee Symes <leesdolphin@users.sourceforge.net>
2014-04-28 11:41:12 +1000

    Reformatted Guava tests. Also added a sorted set test suite.

--- Desktop/DLect-Events/src/collectionTest/java/org/dlect/events/collections/EventFiringSortedSetGuavaTest.java
+++ Desktop/DLect-Events/src/collectionTest/java/org/dlect/events/collections/EventFiringSortedSetGuavaTest.java
@@ -5,14 +5,15 @@
  */
 package org.dlect.events.collections;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.testing.ListTestSuiteBuilder;
-import com.google.common.collect.testing.TestStringListGenerator;
+import com.google.common.collect.Sets;
+import com.google.common.collect.testing.SortedSetTestSuiteBuilder;
+import com.google.common.collect.testing.TestStringSortedSetGenerator;
 import com.google.common.collect.testing.features.CollectionFeature;
 import com.google.common.collect.testing.features.CollectionSize;
-import com.google.common.collect.testing.features.ListFeature;
+import com.google.common.collect.testing.features.SetFeature;
 import java.util.Arrays;
-import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import junit.framework.Test;
 import junit.framework.TestCase;
 
@@ -20,41 +21,37 @@ import junit.framework.TestCase;
  *
  * @author lee
  */
-public class EventFiringListGuavaTest extends TestCase {
+public class EventFiringSortedSetGuavaTest extends TestCase {
 
     public static Test suite() {
-        return ListTestSuiteBuilder
-                .using(new TestStringListGenerator() {
+        return SortedSetTestSuiteBuilder
+                .using(new TestStringSortedSetGenerator() {
 
                     @Override
-                    protected List<String> create(String[] elements) {
-                        EventFiringList<String> l = new EventFiringList<>(Lists.<String>newArrayList(), new NoOpCollectionEventHelper<String>());
-                        l.addAll(Arrays.asList(elements));
-                        return l;
+                    protected SortedSet<String> create(String[] entries) {
+                        TreeSet<String> s = Sets.newTreeSet();
+                        s.addAll(Arrays.asList(entries));
+                        return new EventFiringSortedSet<>(s, new NoOpCollectionEventHelper<String>());
                     }
 
                 })
                 // You can optionally give a name to your test suite. This
                 // name is used by JUnit and other tools during report
                 // generation.
-                .named("EventFiringList tests")
+                .named("EventFiringSortedSet tests")
                 // Guava has a host of "features" in the
                 // com.google.common.collect.testing.features package. Use
                 // them to specify how the collection should behave, and
                 // what operations are supported.
                 .withFeatures(
-                        ListFeature.GENERAL_PURPOSE,
-                        ListFeature.SUPPORTS_ADD_WITH_INDEX,
-                        ListFeature.SUPPORTS_REMOVE_WITH_INDEX,
-                        ListFeature.SUPPORTS_SET,
-                        CollectionFeature.SUPPORTS_ADD,
-                        CollectionFeature.SUPPORTS_REMOVE,
-                        CollectionFeature.SUPPORTS_ITERATOR_REMOVE,
-                        CollectionFeature.ALLOWS_NULL_VALUES,
                         CollectionFeature.ALLOWS_NULL_QUERIES,
+                        CollectionFeature.ALLOWS_NULL_VALUES,
                         CollectionFeature.FAILS_FAST_ON_CONCURRENT_MODIFICATION,
                         CollectionFeature.GENERAL_PURPOSE,
+                        CollectionFeature.KNOWN_ORDER,
+                        CollectionFeature.DESCENDING_VIEW,
                         CollectionFeature.SUBSET_VIEW,
+                        SetFeature.GENERAL_PURPOSE,
                         CollectionSize.ANY
                 ).createTestSuite();
     }
